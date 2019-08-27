19e8211229eb6caae209632b5eaa99c96aaab088
thepg <eboutahir11@gmail.com>
2019-08-27 10:55:06 +0100

    commit

---
+++ src/main/java/com/springreact/projecttest/projecttest/ProjecttestApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.projecttest.projecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttestApplication.class, args);
+	}
+
+}
